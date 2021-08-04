03e1bd1fa2d394259d9df7d320ad9e4d0b5d7bb6
cmingg <chooo12343@gmail.com>
2021-08-04 20:18:56 +0900

    커뮤니티 레이아웃 구현중

--- src/main/java/com/camping/bit/controller/CommunityController.java
+++ src/main/java/com/camping/bit/controller/CommunityController.java
@@ -5,12 +5,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value = "/community/*")
 public class CommunityController {
 
-	@RequestMapping(value = "community.do", method = RequestMethod.GET)
-	public String communityboard() {
+	// 커뮤니티 누르면 첫화면
+	@RequestMapping(value = "main.do", method = RequestMethod.GET)
+	public String mainboard() {
 	
 	return "community.tiles";
 	
 	}
+	
+	// 가입인사게시판
+	@RequestMapping(value = "hello.do", method = RequestMethod.GET)
+	public String helloboard() {
+	
+	return "hello.tiles";
+	
+	}
+	
+	// 게시판 글쓰기
+	@RequestMapping(value = "boardwrite.do", method = RequestMethod.GET)
+	public String boardwrite() {
+	
+	return "boardwrite.tiles";
+	}
+	
 }
