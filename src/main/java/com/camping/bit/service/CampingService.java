03a2124fd589823ccd89da25624fe6a3d5a4a496
hkyep <haybaqwer@gmail.com>
2021-08-04 20:12:57 +0900

    map, pagination

--- src/main/java/com/camping/bit/service/CampingService.java
+++ src/main/java/com/camping/bit/service/CampingService.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import com.camping.bit.dto.CampingDetailDto;
 import com.camping.bit.dto.CampingListDto;
+import com.camping.bit.dto.CampingParam;
 
 public interface CampingService {
 
 	//캠핑장 전체 리스트
-	public List<CampingListDto> getCampingList(CampingListDto dto);
+	public List<CampingListDto> getCampingList(CampingParam param);
 	
 	//각 캠핑장별 상세 화면
 	public CampingDetailDto getCampingDetail(int contentid);
+	
+	//캠핑장 갯수 구하기 
+	public int getCampingCount(CampingParam param);
 }
