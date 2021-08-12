dff90ca6866267f71e8054763198b28b0803dbe8
hkyep <haybaqwer@gmail.com>
2021-08-12 16:00:42 +0900

    campingparam 추가

--- src/main/java/com/camping/bit/dto/CampingParam.java
+++ src/main/java/com/camping/bit/dto/CampingParam.java
@@ -1,28 +1,55 @@
 package com.camping.bit.dto;
 
 import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Arrays;
+
+import org.springframework.web.util.UriComponentsBuilder;
 
 public class CampingParam implements Serializable{
 	
 	private int pageNumber;
+
 	private int start;
 	private int end;
+	
 	private String sorting;
+	
 	private String[] searchDo;
+	private String[] searchCl;
+	private String[] lctCl;
+	private String[] induty;
+	private String[] resveCl;
+	private String eqpmnLendCl;
+	private String exprnProgrm;
+	private String[] animalCmgCl;
+	private String[] sbrsCl;
 	
 	public CampingParam() {
 		
 	}
 
-	public CampingParam(int pageNumber, int start, int end, String sorting, String[] searchDo) {
+
+	public CampingParam(int pageNumber, int start, int end, String sorting, String[] searchDo, String[] searchCl,
+			String[] lctCl, String[] induty, String[] resveCl, String eqpmnLendCl, String exprnProgrm,
+			String[] animalCmgCl, String[] sbrsCl) {
 		super();
 		this.pageNumber = pageNumber;
 		this.start = start;
 		this.end = end;
 		this.sorting = sorting;
 		this.searchDo = searchDo;
+		this.searchCl = searchCl;
+		this.lctCl = lctCl;
+		this.induty = induty;
+		this.resveCl = resveCl;
+		this.eqpmnLendCl = eqpmnLendCl;
+		this.exprnProgrm = exprnProgrm;
+		this.animalCmgCl = animalCmgCl;
+		this.sbrsCl = sbrsCl;
 	}
+
 
 	public int getPageNumber() {
 		return pageNumber;
@@ -64,10 +91,79 @@ public class CampingParam implements Serializable{
 		this.searchDo = searchDo;
 	}
 
-	@Override
-	public String toString() {
-		return "CampingParam [pageNumber=" + pageNumber + ", start=" + start + ", end=" + end + ", sorting=" + sorting
-				+ ", searchDo=" + Arrays.toString(searchDo) + "]";
+	public String[] getSearchCl() {
+		return searchCl;
 	}
 
+	public void setSearchCl(String[] searchCl) {
+		this.searchCl = searchCl;
+	}
+
+	public String[] getLctCl() {
+		return lctCl;
+	}
+
+	public void setLctCl(String[] lctCl) {
+		this.lctCl = lctCl;
+	}
+
+
+	public String[] getInduty() {
+		return induty;
+	}
+
+
+	public void setInduty(String[] induty) {
+		this.induty = induty;
+	}
+
+
+	public String[] getResveCl() {
+		return resveCl;
+	}
+
+
+	public void setResveCl(String[] resveCl) {
+		this.resveCl = resveCl;
+	}
+
+
+	public String getEqpmnLendCl() {
+		return eqpmnLendCl;
+	}
+
+
+	public void setEqpmnLendCl(String eqpmnLendCl) {
+		this.eqpmnLendCl = eqpmnLendCl;
+	}
+
+
+	public String getExprnProgrm() {
+		return exprnProgrm;
+	}
+
+
+	public void setExprnProgrm(String exprnProgrm) {
+		this.exprnProgrm = exprnProgrm;
+	}
+
+
+	public String[] getAnimalCmgCl() {
+		return animalCmgCl;
+	}
+
+
+	public void setAnimalCmgCl(String[] animalCmgCl) {
+		this.animalCmgCl = animalCmgCl;
+	}
+
+
+	public String[] getSbrsCl() {
+		return sbrsCl;
+	}
+
+
+	public void setSbrsCl(String[] sbrsCl) {
+		this.sbrsCl = sbrsCl;
+	}
 }
